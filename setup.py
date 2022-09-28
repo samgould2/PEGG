9d0bacf39b6a2eb01b339376c722064097a71ac5
Sam Gould <80423022+samgould2@users.noreply.github.com>
2022-09-28 15:24:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,16 @@ setup(
     name = 'pegg',
     author = 'Samuel Gould',
     author_email = 'samgould@mit.edu',
+    url = 'https://github.com/samgould2/PEGG',
     version = '0.0.1',
     description = 'Prime Editing Guide Generator',
     py_modules = ["pegg"],
-    package_dir = {'': 'src'}
+    package_dir = {'': 'src'},
 
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.9",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+        ]
 )
