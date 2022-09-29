c278eb1672f0e7539e33c6ff146cc9ab270e3d2f
Sam Gould <80423022+samgould2@users.noreply.github.com>
2022-09-29 15:14:58 -0400

    Update conf.py

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -16,9 +16,7 @@ import mock
 import sphinx.ext.autodoc
 #import nltk_theme
 sys.path.insert(0, os.path.abspath('..'))
-#autodoc_mock_imports = ['numpy', 'numpy.core.multiarray','matplotlib.pyplot','pandas',
-#'Bio', 'gzip', 'Bio.Align','Bio.pairwise2','matplotlib.colors',
-#'seaborn','matplotlib.patches','re','seaborn']
+autodoc_mock_imports = ['numpy','matplotlib','pandas','Bio', 'gzip', 'seaborn','re','seaborn']
 
 # -- Project information -----------------------------------------------------
 
